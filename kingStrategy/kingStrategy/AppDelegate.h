37c82fc04c1137217b1055a64082b4a2eaae8681
EricChen <chenzhihao.exe@gmail.com>
2017-03-29 14:14:10 +0800

    new project

---
+++ kingStrategy/kingStrategy/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kingStrategy
+//
+//  Created by eric on 17/3/29.
+//  Copyright © 2017年 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kingStrategy/kingStrategy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kingStrategy
+//
+//  Created by eric on 17/3/29.
+//  Copyright © 2017年 eric. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
